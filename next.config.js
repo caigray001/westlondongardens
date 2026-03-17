52ccf91ca0735976149c9fa3756bffb95ac37041
caigray001 <admin@sprintspeak.com>
2026-03-17 18:52:26 +0000

    commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,23 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://sprintspeak.com https://www.sprintspeak.com",
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL',
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
